08a0211960cd486754952548a1809c9b57f07009
Mina S. Aziz <minaz922@gmail.com>
2017-04-22 23:38:26 +0200

    create Book component

---
+++ src/components/book/BookPage.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+class Book extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  submitBook(input) {
+    alert('Submitted');
+  }
+
+  render() {
+    let titleInput;
+
+    return(
+      <div>
+        <h3>Books</h3>
+        <ul>
+          {/* traverse books array */}
+          {this.props.books.map((b, i) => <li key={i}>{b.title}</li>)};
+        </ul>
+        <div>
+          <h3>Books Form</h3>
+          <form onSubmit={e => {
+            e.preventDefault();
+            let input = {title: titleInput.value};
+            this.submitBook(input);
+            e.target.reset();
+          }}>
+            <input type="text" name="title" ref={node => titleInput = node} />
+            <input type="submit" />
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Book;
