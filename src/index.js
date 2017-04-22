fab7fcfba02cef1b2b5df1512aca494f389de124
Mina S. Aziz <minaz922@gmail.com>
2017-04-23 00:15:53 +0200

    install and import bootstrap

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import {render} from 'react-dom';
 import {Router, browserHistory} from 'react-router';
 import routes from './routes';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 render(
   <Router routes={routes} history={browserHistory} />,
