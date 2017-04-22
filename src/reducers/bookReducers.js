b9b7c32944b75acade7cb90972dc4f7ffa71a6df
Mina S. Aziz <minaz922@gmail.com>
2017-04-23 00:06:23 +0200

    tell liner to ignore missing 'break' in reducer switch statement

--- src/reducers/bookReducers.js
+++ src/reducers/bookReducers.js
@@ -2,6 +2,7 @@ export default (state = [], action) => {
   switch (action.type) {
     case 'CREATE_BOOK':
       state.push(action.book);
+      /* falls through */
     default:
       return state;
   }
